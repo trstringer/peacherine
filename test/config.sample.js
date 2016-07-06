0f85b94cb445bfc094a6d592e15ddfdfc908658e
Thomas Stringer <tr.stringer@outlook.com>
2016-07-06 16:34:03 -0400

    add sample config module for testing

---
+++ test/config.sample.js
@@ -0,0 +1,29 @@
+// this is a sample config module that you can use as a 
+// starting base to run the unit tests. all you should do 
+// with this is the following:
+//
+//  1. copy/rename to config.js (keep it in test folder)
+//  2. input your environment-specific database
+//  3. run the tests
+
+module.exports = {
+  connections: [
+    {
+      dataSourceType: 'mssql',
+      server: '...',
+      database: '...',
+      username: '...',
+      password: '...'
+    },
+    {
+      dataSourceType: 'documentdb',
+      endpoint: '...',
+      key: '...',
+      // the test requires a collection to exist 
+      // named testcollection01 in your ddb database
+      // 
+      // todo: remove this non-default test dependency
+      database: '...'
+    }
+  ]
+};

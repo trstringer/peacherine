470ebad9d9ff7498f3dd332cbcb0b5513812e486
Thomas Stringer <tr.stringer@outlook.com>
2016-07-05 21:51:40 -0400

    add boilerplate for documentdb connector

---
+++ src/documentdb-connector.js
@@ -0,0 +1,16 @@
+const DocumentClient = require('documentdb').DocumentClient;
+
+module.exports = (() => {
+  const testConnection = (connectionOptions, callback) => {
+
+  };
+
+  const query = (connectionOptions, queryOptions, callback) => {
+    
+  };
+
+  return {
+    testConnection,
+    query
+  };
+})();
